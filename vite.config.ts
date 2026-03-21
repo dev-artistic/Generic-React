82e14ed0f3b6b6caab88ddc5c2bf487ce0ed78ad
dev-artistic <dev.artistic.01@gmail.com>
2026-03-22 00:28:08 +0530

    react switch (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [react(), tailwindcss()],
 })
